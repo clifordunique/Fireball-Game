477d64588aa6edc27348d6fc04be536e2e82dfa3
John Paul Depew <jpdepewjp@gmail.com>
2017-07-28 21:19:40 -0400

    Rhino detects player with raycast

--- Assets/Scripts/Rhino.cs
+++ Assets/Scripts/Rhino.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Rhino : MonoBehaviour {
 
-    Player player;
+    public LayerMask layerMask;
     public Transform head;
-    Animator anim;
+    public Transform eye;
     public float easeAmount;
     public float rotateUpTarget = -10f;
     public float rotateDownTarget = 10;
@@ -21,6 +21,8 @@ public class Rhino : MonoBehaviour {
     float velocityXSmoothing;
     float moveSpeed = 0;
 
+    Player player;
+    Animator anim;
     RhinoController controller;
     Vector2 velocity;
 
@@ -38,15 +40,6 @@ public class Rhino : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //Debug.Log(head.rotation.z * Mathf.Rad2Deg);
-        //Debug.Log(Mathf.Abs(player.transform.position.x - transform.position.x));
-        if (Mathf.Abs(player.transform.position.x - transform.position.x) < 20)
-        {
-            if (seePlayerEvent != null)
-            {
-                seePlayerEvent();
-            }
-        }
 
         // Velocity on y axis reset if collision above or below player
         if (controller.collisions.above || controller.collisions.below)
@@ -62,6 +55,20 @@ public class Rhino : MonoBehaviour {
         }
         CalculateVelocity();
         controller.Move(velocity, false);
+        CanSeePlayer();
+    }
+
+    void CanSeePlayer()
+    {
+        Vector2 directionToPlayer = player.transform.position - eye.transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(eye.transform.position, directionToPlayer, 30, layerMask);
+        if (hit && hit.transform.gameObject.layer == 10)
+        {
+            if (seePlayerEvent != null)
+            {
+                seePlayerEvent();
+            }
+        }
     }
 
     void SeePlayer()
@@ -83,7 +90,6 @@ public class Rhino : MonoBehaviour {
             rotationPercentage = currentRotation / totalRotation;
             rotationPercentage = Mathf.Clamp01(rotationPercentage);
             float easedPercentBetweenRotation = -Ease(rotationPercentage);
-            Debug.Log(head.rotation.z * Mathf.Rad2Deg + " " + (rotateUpTarget + 2));
             head.Rotate(Vector3.forward, easedPercentBetweenRotation * rotationSpeed * Time.deltaTime);
 
             yield return null;
@@ -99,7 +105,7 @@ public class Rhino : MonoBehaviour {
         float currentRotation;
         float rotationPercentage;
 
-        while (head.rotation.z * Mathf.Rad2Deg < rotateDownTarget - 2)
+        while (head.rotation.z * Mathf.Rad2Deg < rotateDownTarget - 3)
         {
             currentRotation = Mathf.Abs(rotateDownTarget - head.rotation.z * Mathf.Rad2Deg);
             rotationPercentage = currentRotation / totalRotation;
@@ -140,12 +146,12 @@ public class Rhino : MonoBehaviour {
         return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1 - x, a));
     }
 
+    /* Calculates x and y velocity
+     */
     void CalculateVelocity()
     {
-
         float targetVelocityX = moveSpeed * Mathf.Sign(chargeDirection);
         velocity.y += gravity * Time.deltaTime;
-        Debug.Log(velocity.y + " " + moveSpeed);
         velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTime);
     }
 }
