e5cc383b2f4f542d517ec2c34d1708d02d496744
John Paul Depew <jpdepewjp@gmail.com>
2018-03-29 12:51:54 -0700

    Created HealthBar script
    This script changes the value in the healthbar.  It will need to be integrated with the actual health of the player.

---
+++ Assets/Scripts/UI/Healthbar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+
+    public GameObject coverItem;
+    public Transform pos;
+    public int percent=0;
+    public int offset;
+
+    int count=0;
+    Vector2 position;
+    Stack<GameObject> coverStack;
+
+    void Start()
+    {
+        // initialization
+        position = pos.position;
+        coverStack = new Stack<GameObject>();
+    }
+
+
+    void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    /* This function updates the healthbar UI element by adding
+     * a small black sprite - coverItem to the stack - coverStack
+     * and positioning it in the proper place over the healthbar.
+     */
+    void UpdateHealthBar()
+    {
+        // Player is losing health
+        if (count < percent)
+        {
+            GameObject temp = Instantiate(coverItem, position, coverItem.transform.rotation, this.transform);
+            coverStack.Push(temp);
+            count++;
+            position = new Vector2(position.x - offset, position.y);            // offset is the distance from the olde sprite to where the new sprite will be instantiated
+        }
+        // Player is gaining health
+        if (count > percent)
+        {
+            GameObject temp = coverStack.Pop();
+            Destroy(temp);
+            count--;
+            position = new Vector2(position.x + offset, position.y);
+        }
+    }
+}
