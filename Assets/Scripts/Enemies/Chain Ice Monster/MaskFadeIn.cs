300b48d3423833ba88d66d619049be3c516abae7
John Paul Depew <jpdepewjp@gmail.com>
2018-06-04 09:38:15 -0400

    Ice mask fade in

---
+++ Assets/Scripts/Enemies/Chain Ice Monster/MaskFadeIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteMask))]
+public class MaskFadeIn : MonoBehaviour {
+
+    float startFade = 1f;
+    public float endFade = 0.1f;
+    SpriteMask mask;
+
+	void Start () {
+        mask = GetComponent<SpriteMask>();
+        mask.alphaCutoff = startFade;
+        StartCoroutine(FadeIn());
+	}
+
+    IEnumerator FadeIn()
+    {
+        while (mask.alphaCutoff > endFade)
+        {
+            mask.alphaCutoff = Mathf.Lerp(mask.alphaCutoff, mask.alphaCutoff - 0.1f, 1f);
+            yield return null;
+        }
+    }
+}
