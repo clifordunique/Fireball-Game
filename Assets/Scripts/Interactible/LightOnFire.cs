7819721013343bf3d2064bf33b9ea50bc7b89b3a
John Paul Depew <jpdepewjp@gmail.com>
2018-07-10 23:00:30 -0400

    Golem 1 fire eyes

--- Assets/Scripts/Interactible/LightOnFire.cs
+++ Assets/Scripts/Interactible/LightOnFire.cs
@@ -1,33 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LightOnFire : MonoBehaviour
 {
-    public GameObject glowObject;
     public GameObject actionObject;
-
-    protected virtual void Start()
-    {
-        Color color = glowObject.GetComponent<SpriteRenderer>().color;
-        glowObject.GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 0);
-    }
-
-    protected virtual void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
-        {
-            Utilities.instance.FadeObjectIn(glowObject, 0.05f);
-        }
-    }
-
-    protected virtual void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
-        {
-            Utilities.instance.FadeObjectOut(glowObject, 0.05f, false, false);
-        }
-    }
 
     public virtual void FireAction()
     {
---
+++ Assets/Scripts/Interactible/TriggerSensor/TSFadeInOut.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TSFadeInOut : TriggerSensor {
+
+    public override void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Utilities.instance.FadeObjectIn(actionObject.gameObject, 0.05f);
+        }
+    }
+
+    public override void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Utilities.instance.FadeObjectOut(actionObject.gameObject, 0.05f, false, false);
+        }
+    }
+}
